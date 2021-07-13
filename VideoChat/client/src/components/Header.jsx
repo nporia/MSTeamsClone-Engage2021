85fb0722051d4ae6585b038dfe52a19816334600
nporia <Porianikita262@gmail.com>
2021-07-13 15:10:36 +0530

    Add files via upload

---
+++ VideoChat/client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <span className="left">
+
+         &nbsp;
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/480px-Microsoft_logo.svg.png" />
+        MICROSOFT
+      </span>
+      <span className="right">
+        <a href="https://microsoft-teams-clone-index.netlify.app/"> Teams </a>
+      </span>
+
+    </header>
+  );
+}
+
+export default Header;
