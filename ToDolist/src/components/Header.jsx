59d60c950cca033137932a801b01efd59c857a52
nporia <Porianikita262@gmail.com>
2021-07-13 14:49:47 +0530

    Add files via upload

---
+++ ToDolist/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+      <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/480px-Microsoft_logo.svg.png" />
+       MICROSOFT
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
